f87928b5b0cda33333aded3f6965401b81456818
ravialiaswire <ravikumar.7oct@gmail.com>
2015-10-06 18:12:53 +0530

    fdfd

---
+++ ravi/Test.java
@@ -0,0 +1,24 @@
+package techm;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+	driver.get("http://localhost:8085/SpringWSConsumer/");
+	driver.findElement(By.id("a")).sendKeys("34");
+	driver.findElement(By.id("b")).sendKeys("4");
+	driver.findElement(By.id("c")).click();
+	
+		
+	}
+
+}
+
