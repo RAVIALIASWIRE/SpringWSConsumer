1a46cd9b6a733949eb47ef550737689d4745ed31
TECHMOJO <TECHMOJO@Ravi>
2015-11-25 18:30:25 +0530

    dfdfdf

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -11,7 +11,7 @@ public class Test {
 	public static void main(String[] args) {
 	WebDriver driver = new FirefoxDriver();
 	driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-	driver.get("http://localhost:8085/SpringWSConsumer/");
+	driver.get("http://localhost:8086/SpringWSConsumer/");
 	driver.findElement(By.id("a")).sendKeys("34");
 	driver.findElement(By.id("b")).sendKeys("4");
 	driver.findElement(By.id("c")).click();
